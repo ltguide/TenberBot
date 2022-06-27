686613a5a46fe6a98a50197f73b495f5f79f8571
Matthew Churilla <ltguide@gmail.com>
2022-06-26 22:45:58 -0400

    basic bot

---
+++ Solution/TenberBot/Modules/InfoModule.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenberBot.Modules;
+public class InfoModule : ModuleBase<SocketCommandContext>
+{
+	// ~say hello world -> hello world
+	[Command("say")]
+	[Summary("Echoes a message.")]
+	public Task SayAsync([Remainder][Summary("The text to echo")] string echo)
+		=> ReplyAsync(echo);
+}
