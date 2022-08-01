19825e48fe80744cdd7ba1c370245612b102fae3
Matthew Churilla <ltguide@gmail.com>
2022-08-01 00:18:45 -0400

    cache temperature-inline and tweak regex

--- Solution/TenberBot/Modules/Command/ConvertCommandModule.cs
+++ Solution/TenberBot/Modules/Command/ConvertCommandModule.cs
@@ -1,8 +1,12 @@
 ﻿using Discord;
 using Discord.Commands;
+using Microsoft.Extensions.Caching.Memory;
 using System.Text.RegularExpressions;
 using TenberBot.Attributes;
+using TenberBot.Extensions;
 using TenberBot.Results.Command;
+using UnitsNet;
+using UnitsNet.Units;
 
 namespace TenberBot.Modules.Command;
 
@@ -11,11 +15,14 @@ namespace TenberBot.Modules.Command;
 [RequireBotPermission(ChannelPermission.SendMessages)]
 public class ConvertCommandModule : ModuleBase<SocketCommandContext>
 {
+    private readonly IMemoryCache memoryCache;
     private readonly ILogger<ConvertCommandModule> logger;
 
     public ConvertCommandModule(
+        IMemoryCache memoryCache,
         ILogger<ConvertCommandModule> logger)
     {
+        this.memoryCache = memoryCache;
         this.logger = logger;
     }
 
@@ -30,24 +37,49 @@ public class ConvertCommandModule : ModuleBase<SocketCommandContext>
     [Alias("temp")]
     [Summary("Convert temperatures.\nInclude the unit (C or F) to convert from.")]
     [Remarks("`<temp>`")]
-    [InlineTrigger(@"\b(-?\d+(?:\.\d+)?)°? ?([CF])\b", RegexOptions.IgnoreCase)]
     public async Task<RuntimeResult> Temp([Remainder] string? word = null)
     {
         var match = Regex.Match(word ?? "", @"(-?\d+(?:\.\d+)?)°? ?([CF])", RegexOptions.IgnoreCase);
         if (match.Success == false)
-            return DeleteResult.FromError("Please provide a temperature to convert, e.g. `86F` or `46.5° C`");
+            return DeleteResult.FromError("Please provide a temperature to convert, e.g. `86F` or `46.5°C`");
 
         var value = double.Parse(match.Groups[1].Value);
-        var unit = match.Groups[2].Value.ToUpper();
+        var unit = match.Groups[2].Value;
 
-        (double Value, string Unit) result;
-        if (unit == "C")
-            result = ((value * 1.8d) + 32, "F");
-        else
-            result = ((value - 32) * (5d / 9d), "C");
+        var (from, to) = ConvertTemperatures(value, unit);
 
-        await Context.Message.ReplyAsync($"{value:#,##0.##}° {unit} is {result.Value:#,##0.##}° {result.Unit}");
+        await Context.Message.ReplyAsync($"{from} is {to}");
 
         return DeleteResult.FromSuccess();
+    }
+
+    [Command("temperature-inline")]
+    [InlineTrigger(@"(?:^|[^\w.])(-?\d+(?:\.\d+)?)°? ?([CF])(?=\W*(?:\s|$)|$)", RegexOptions.IgnoreCase)]
+    public async Task TempInline(double value, string unit)
+    {
+        var (from, to) = ConvertTemperatures(value, unit);
+
+        var key = $"temperature-inline, {Context.Channel.Id}, {(from.Unit == TemperatureUnit.DegreeCelsius ? from : to)}";
+
+        if (memoryCache.TryGetValue<Temperature>(key, out var _))
+            return;
+
+        memoryCache.Set(key, from, TimeSpan.FromMinutes(30));
+
+        await Context.Message.ReplyAsync($"{from} is {to}");
+    }
+
+    private static (Temperature From, Temperature To) ConvertTemperatures(double value, string unit)
+    {
+        if (unit.ToUpper() == "C")
+        {
+            var from = Temperature.FromDegreesCelsius(value);
+            return (from, from.ToUnit(TemperatureUnit.DegreeFahrenheit));
+        }
+        else
+        {
+            var from = Temperature.FromDegreesFahrenheit(value);
+            return (from, from.ToUnit(TemperatureUnit.DegreeCelsius));
+        }
     }
 }
